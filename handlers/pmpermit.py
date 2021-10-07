7607faa0d52374918c4b1bf6b504eaee22531154
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-10-07 18:31:34 +0300

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,6 +5,6 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Merhaba, Bu bir müzik asistanı hizmetidir.\n\n ❗️ kurallar:\n   - Sohbete izin yok\n   - İstenmeyen postaya izin verilmez \n\n 🚨 **USERBOT GRUBUNUZA KATILAMAZSA GRUP DAVETI BAĞLANTISI VEYA KULLANICI ADI GÖNDER.**\n\n ⚠️ DİKKAT: Burada bir mesaj gönderiyorsanız Yöneticinin iletinizi göreceği anlamına gelir. Msj bırakabilir veya kısa sorular sorabilirsiniz.\n    - Bu kullanıcıyı gizli gruplara eklemeyin. Userbot gizli grublara /asistan komutu ile gelemeyebilir.\n   - Özel bilgileri burada paylaşmayın\n\n")
+  await USER.send_message(message.chat.id,"Merhaba, Bu bir müzik asistanı hizmetidir.\n\n ❗️ kurallar:\n   - Sohbete izin yok\n   - İstenmeyen postaya izin verilmez \n\n 🚨 **USERBOT GRUBUNUZA KATILAMAZSA GRUP DAVETİ BAĞLANTISI VEYA KULLANICI ADI GÖNDERİNİZ.**\n\n ⚠️ DİKKAT: Burada bir mesaj gönderiyorsunuz. Yöneticinin iletinizi göreceği anlamına gelir. Msj bırakabilir veya kısa sorular sorabilirsiniz.\n  - Bu kullanıcıyı gizli gruplara eklemeyin. Userbot gizli grublara /asistan komutu ile gelemeyebilir.\n   - Özel bilgileri burada paylaşmayın\n\n")
   return                        
  
